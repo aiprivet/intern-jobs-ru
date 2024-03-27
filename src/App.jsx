9aad47214a6d65d7b4103f2a22e39319724efb7c
Alexandr <aiprivet@icloud.com>
2024-03-28 02:14:30 +0300

    make start screen layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,61 @@
 import { DiCode } from "react-icons/di";
 import { TbBrandJavascript } from "react-icons/tb";
+import { ImDatabase } from "react-icons/im";
+import { BiMobile } from "react-icons/bi";
+import { SiPostman } from "react-icons/si";
+import { FaLinux } from "react-icons/fa";
+import { SiSimpleanalytics } from "react-icons/si";
+import { FaBriefcase } from "react-icons/fa";
+import { GiArtificialIntelligence } from "react-icons/gi";
 
 import Button from "./ui/Button";
 function App() {
   return (
-    <>
-      <header className="flex justify-between items-center border-b border-neutral-300">
-        <div className="text-slate-800 flex items-center pl-2 font-semibold">
+    <div className="h-screen bg-neutral-50 dark:bg-slate-950 w-screen">
+      <header className="flex justify-between items-center border-b dark:border-slate-900 border-neutral-300">
+        <div className="text-slate-800 flex items-center pl-2 font-semibold dark:text-slate-100">
           <DiCode size="48"></DiCode>
-          <span>Стажировки для IT специалистов</span>
+          <span>IT Стажировки в РФ</span>
         </div>
       </header>
-      <main className="flex items-center justify-center mt-12 text-slate-800 flex-col">
-        <h1 className="text-center font-semibold text-3xl">
+      <main className="flex items-center justify-center mt-12 text-slate-800 dark:text-slate-100 flex-col">
+        <h1 className="text-center font-semibold text-4xl">
           Найди стажировку по своему направлению
         </h1>
-        <div className="mt-4">
+        <div className="mt-8 flex gap-2 max-w-2xl flex-wrap justify-center">
           <Button
             color={"yellow"}
             icon={<TbBrandJavascript></TbBrandJavascript>}
           >
             Frontend
           </Button>
+          <Button color={"gray"} icon={<ImDatabase></ImDatabase>}>
+            Backend
+          </Button>
+          <Button color={"blue"} icon={<BiMobile></BiMobile>}>
+            Mobile
+          </Button>
+          <Button color={"green"} icon={<SiPostman></SiPostman>}>
+            QA
+          </Button>
+          <Button color={"purple"} icon={<FaLinux></FaLinux>}>
+            DevOps
+          </Button>
+          <Button color={"blue"} icon={<SiSimpleanalytics></SiSimpleanalytics>}>
+            Data analysis
+          </Button>
+          <Button color={"purple"} icon={<FaBriefcase></FaBriefcase>}>
+            Product manager
+          </Button>
+          <Button
+            color={"gray"}
+            icon={<GiArtificialIntelligence></GiArtificialIntelligence>}
+          >
+            Machine learning
+          </Button>
         </div>
       </main>
-    </>
+    </div>
   );
 }
 
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ children, color, onClick, icon }) {
     red: "bg-red-700 hover:bg-red-800 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 focus:ring-red-300",
     gray: "bg-gray-700 hover:bg-gray-800 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 focus:ring-gray-300",
     yellow:
-      "bg-yellow-700 hover:bg-yellow-800 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800 focus:ring-yellow-300",
+      "bg-yellow-500 hover:bg-yellow-600 dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:focus:ring-yellow-800 focus:ring-yellow-300",
     green:
       "bg-green-700 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 focus:ring-green-300",
     purple:
@@ -16,7 +16,7 @@ export default function Button({ children, color, onClick, icon }) {
       onClick={onClick ? onClick : () => {}}
       className={`${
         icon ? "flex items-center gap-1" : ""
-      } text-white focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ${
+      }  cursor-pointer text-white focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ${
         colorClassNames[color]
       }`}
     >
