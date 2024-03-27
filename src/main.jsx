2bcae9cee2558a213ee9409c39a4ce48486227fe
Alexandr <aiprivet@icloud.com>
2024-03-28 00:59:51 +0300

    delete extra files & folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />} path="/" />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
