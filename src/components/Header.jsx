3c9b7617b55e5914aef1635fe8959ff3245c89c0
Alexandr <aiprivet@icloud.com>
2024-03-31 15:57:51 +0300

    update logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
-import { DiCode } from "react-icons/di";
 import { FaGithub } from "react-icons/fa";
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center border-b dark:border-slate-900 border-neutral-300">
       <div className="flex text-slate-800  items-center pl-2 font-semibold dark:text-slate-100">
-        <DiCode size="48"></DiCode>
-        <span>IT Стажировки в РФ</span>
+        <span className="text-2xl p-2">👩🏼‍💻</span>
+        <span className="text-transparent bg-clip-text bg-gradient-to-br from-yellow-200 to-orange-500">
+          IT Стажировки в РФ
+        </span>
       </div>
       <a href="https://github.com/aiprivet/intern-jobs-ru">
-        <div className="text-white text-2xl mr-2">
+        <div className="text-white text-2xl p-2">
           <FaGithub />
         </div>
       </a>
