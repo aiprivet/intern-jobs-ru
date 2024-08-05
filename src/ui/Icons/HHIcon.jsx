0dc71a37dfa9e7731b3f39b3016656f6646c5711
Alexandr <aiprivet@icloud.com>
2024-08-05 16:57:35 +0300

    fix hh icon

--- src/ui/Icons/HHIcon.jsx
+++ src/ui/Icons/HHIcon.jsx
@@ -1,3 +1,4 @@
+import img from '/public/min-hh-red.png'
 export default function HHIcon() {
-  return <img width={48} height={48} src="/public/min-hh-red.png"></img>;
+  return <img width={48} height={48} src={img}></img>;
 }
